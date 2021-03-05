91883e4b9a37bacf1338bbaf11006b7c71845510
mf_Mii <mf-Mii@outlook.jp>
2021-03-06 02:39:23 +0900

    First commit

--- src/main/java/work/mfmii/other/ASM_System/command/Help.java
+++ src/main/java/work/mfmii/other/ASM_System/command/Help.java
@@ -1,7 +1,16 @@
 package work.mfmii.other.ASM_System.command;
 
-public class Help {
-    public Help(){
+import net.dv8tion.jda.api.entities.User;
+import org.jetbrains.annotations.NotNull;
+import work.mfmii.other.ASM_System.utils.CommandManager;
 
+public class Help extends CommandManager {
+    public Help(String name){
+        super(name);
+    }
+
+    @Override
+    public boolean execute(@NotNull User sender, @NotNull String command, @NotNull String[] args) {
+        return true;
     }
 }
